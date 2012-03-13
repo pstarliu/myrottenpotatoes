b673632b9f6d71875a1126c92db1fedf53306d03
Paul Liu <pstar.liu@gmail.com>
2012-03-14 09:33:24 +1300

    finished chapter 3 code on book

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,5 +32,12 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
 end
 
