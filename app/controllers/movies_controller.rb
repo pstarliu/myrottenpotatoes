d4273a697f6c70edd5c7ff53e3692fd33d465a53
Paul Liu <pstar.liu@gmail.com>
2012-03-23 17:38:12 +1300

    Upto 5.3 but code not fully working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,7 +35,7 @@ class MoviesController < ApplicationController
 
   def search_tmdb
     # hardwire to simulate failure
-    flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
+    @movies = Movie.find_in_tmdb(params[:search_terms])
     redirect_to movies_path
   end
 
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe MoviesController do
   describe 'searching TMDb' do
     it 'should call the model method that performs TMDb search' do
+      fake_results = [mock('Movie'), mock('Movie')]
+      Movie.should_receive(:find_in_tmdb).with('hardware').
+        and_return(fake_results)
       post :search_tmdb, {:search_terms => 'hardware'}
     end
     it 'should select the Search Results template for rendering'
